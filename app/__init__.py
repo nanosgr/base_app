c910d2b295e7d8d98bfe9a4f28e3e5ca84579727
Sebastian Rios <sebatian.g.rios@gmail.com>
2021-08-15 00:44:03 -0300

    Ajustamos el inicio de la aplicación

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,24 @@
 
 from flask import Flask, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from app.config import Config
 
-app = Flask(__name__)
+db = SQLAlchemy()
 
-# En esta sección agregamos los módulos creados para
-# redireccionar las rutas conforme se utiliza la aplicación
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-from app.main.routes import main
-from app.users.routes import users
+    # Inicializamos la conexión a la BD.
+    
+    db.init_app(app)
+    # En esta sección agregamos los módulos creados para
+    # redireccionar las rutas conforme se utiliza la aplicación
+    
+    from app.main.routes import main
+    from app.users.routes import users
 
-app.register_blueprint(main)
-app.register_blueprint(users)
+    app.register_blueprint(main)
+    app.register_blueprint(users)
+
+    return app
